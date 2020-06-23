88bf9362f805a43589ef9dd14d43396f5c5d0457
Brice <napofedoubrice@gmail.com>
2020-06-23 20:47:49 +0200

    Page devis, Front fini

--- src/Controller/DevisController.php
+++ src/Controller/DevisController.php
@@ -2,18 +2,182 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Devis;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DevisController extends AbstractController
 {
     /**
      * @Route("/devis", name="devis")
      */
-    public function index()
+    public function index(Request $request, ObjectManager $manager)
     {
+        $devis = new Devis();
+
+        $form = $this->createFormBuilder($devis)
+                    ->add('prenom', TextType::class)
+                    ->add('nom', TextType::class)
+                    ->add('departement', ChoiceType::class,[
+                        'attr' => [
+                            'value' => 'test'
+                        ],
+                        'choices' => [
+                            'Département' => null,
+                            '01' => '01',
+                            '02' => '02',
+                            '03' => '03',
+                            '04' => '04',
+                            '05' => '05',
+                            '06' => '06',
+                            '07' => '07',
+                            '08' => '08',
+                            '09' => '09',
+                            '10' => '10',
+                            '11' => '11',
+                            '12' => '12',
+                            '13' => '13',
+                            '14' => '14',
+                            '15' => '15',
+                            '16' => '16',
+                            '17' => '17',
+                            '18' => '18',
+                            '19' => '19',
+                            '20' => '20',
+                            '21' => '21',
+                            '22' => '22',
+                            '23' => '23',
+                            '24' => '24',
+                            '25' => '25',
+                            '26' => '26',
+                            '27' => '27',
+                            '28' => '28',
+                            '29' => '29',
+                            '30' => '30',
+                            '31' => '31',
+                            '32' => '32',
+                            '33' => '33',
+                            '34' => '34',
+                            '35' => '35',
+                            '36' => '36',
+                            '37' => '37',
+                            '38' => '38',
+                            '39' => '39',
+                            '40' => '40',
+                            '41' => '41',
+                            '42' => '42',
+                            '43' => '43',
+                            '44' => '44',
+                            '45' => '45',
+                            '46' => '46',
+                            '47' => '47',
+                            '48' => '48',
+                            '49' => '49',
+                            '50' => '50',
+                            '51' => '51',
+                            '52' => '52',
+                            '53' => '53',
+                            '54' => '54',
+                            '55' => '55',
+                            '56' => '56',
+                            '57' => '57',
+                            '58' => '58',
+                            '59' => '59',
+                            '60' => '60',
+                            '61' => '61',
+                            '62' => '62',
+                            '63' => '63',
+                            '64' => '64',
+                            '65' => '65',
+                            '66' => '66',
+                            '67' => '67',
+                            '68' => '68',
+                            '69' => '69',
+                            '70' => '70',
+                            '71' => '71',
+                            '72' => '72',
+                            '73' => '73',
+                            '74' => '74',
+                            '75' => '75',
+                            '76' => '76',
+                            '77' => '77',
+                            '78' => '78',
+                            '79' => '79',
+                            '80' => '80',
+                            '81' => '81',
+                            '82' => '82',
+                            '83' => '83',
+                            '84' => '84',
+                            '85' => '85',
+                            '86' => '86',
+                            '87' => '87',
+                            '88' => '88',
+                            '89' => '89',
+                            '90' => '90',
+                            '91' => '91',
+                            '92' => '92',
+                            '93' => '93',
+                            '94' => '94',
+                            '95' => '95',
+                            '971' => '971',
+                            '972' => '972',
+                            '973' => '973',
+                            '974' => '974',
+                            '976' => '976',
+                            '976' => '976',
+                            '2a' => '2a',
+                            '2b' => '2b'
+                        ]
+                    ])
+                    ->add('email', EmailType::class)
+                    ->add('meubles', ChoiceType::class,[
+                        'choices' => [
+                            'Meubles' => null,
+                            'Table à manger' => 'Table à manger',
+                            'Table basse' => 'Table basse',
+                            'Planche à découper' => 'Planche à découper'
+                        ]
+                    ])
+                    ->add('longueur', IntegerType::class)
+                    ->add('largeur', IntegerType::class)
+                    ->add('typeBois', ChoiceType::class,[
+                        'choices' => [
+                            'Type de bois' => null,
+                            'Sapin' => 'Sapin',
+                            'Chêne' => 'Chêne',
+                            'Pin' => 'Pin'
+                        ]
+                    ])
+                    ->add('budget', ChoiceType::class,[
+                        'choices' => [
+                            'Budget' => null,
+                            'Moins de 500€' => '> 500€',
+                            'Entre 500€ et 1000€' => 'De 500€ à 1000€',
+                            'Entre 1000€ et 2000€' => 'De 1000€ à 2000€',
+                            'Plus de 2000€' => '< 2000€'
+                        ]
+                    ])
+                    ->add('message')
+                    ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $manager->persist($devis);
+            $manager->flush();
+
+            return $this->redirectToRoute('home');
+        }
+
         return $this->render('devis/index.html.twig', [
             "nav_activ" => 'devis',
+            "form" => $form->createView()
         ]);
     }
 }
---
+++ src/Entity/Devis.php
@@ -0,0 +1,195 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\DevisRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraint as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass=DevisRepository::class)
+ */
+class Devis
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $prenom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $departement;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $meubles;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $longueur;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $largeur;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $typeBois;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $budget;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $message;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDepartement(): ?string
+    {
+        return $this->departement;
+    }
+
+    public function setDepartement(string $departement): self
+    {
+        $this->departement = $departement;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getMeubles(): ?string
+    {
+        return $this->meubles;
+    }
+
+    public function setMeubles(string $meubles): self
+    {
+        $this->meubles = $meubles;
+
+        return $this;
+    }
+
+    public function getLongueur(): ?int
+    {
+        return $this->longueur;
+    }
+
+    public function setLongueur(int $longueur): self
+    {
+        $this->longueur = $longueur;
+
+        return $this;
+    }
+
+    public function getLargeur(): ?int
+    {
+        return $this->largeur;
+    }
+
+    public function setLargeur(int $largeur): self
+    {
+        $this->largeur = $largeur;
+
+        return $this;
+    }
+
+    public function getTypeBois(): ?string
+    {
+        return $this->typeBois;
+    }
+
+    public function setTypeBois(?string $typeBois): self
+    {
+        $this->typeBois = $typeBois;
+
+        return $this;
+    }
+
+    public function getBudget(): ?string
+    {
+        return $this->budget;
+    }
+
+    public function setBudget(string $budget): self
+    {
+        $this->budget = $budget;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+}
